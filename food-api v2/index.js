e0f73aebe98b04c3dc81cefc11d922514b83dff5
ClarenceKiva <Clarenceaureliopribadi@gmail.com>
2022-06-05 19:59:20 +0700

    updated index.js
    Added additional data for the MySQL pool

--- food-api v2/index.js
+++ food-api v2/index.js
@@ -31,5 +31,6 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    Host: process.env.DB_HOST,
+    host: process.env.INSTANCE_CONNECTION_NAME,
+    port: process.env.DB_PORT
 });

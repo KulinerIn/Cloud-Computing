872d04ccc57e055bbbb8fd2bd94786b8c11586d5
ClarenceKiva <Clarenceaureliopribadi@gmail.com>
2022-06-08 16:37:30 +0700

    Changed the ingredient query

--- food-api v2/index.js
+++ food-api v2/index.js
@@ -42,7 +42,7 @@ function success_response(result) {
 }
 
 function get_recipe_ingredients(food){
-    food = "SELECT ri.ingredient_id AS id, i.name, ri.amount FROM recipe_ingredient ri JOIN ingredient i USING (id) WHERE ri.recipe_id = r.id"
+    food = "SELECT ri.ingredient_id AS id, i.name, ri.amount FROM recipe_ingredient ri JOIN ingredient i ON ri.ingredient_id = i.id JOIN recipe r ON ri.recipe_id = r.id WHERE ri.recipe_id = r.id"
 }
 
 // const query = await db.query(
@@ -83,3 +83,5 @@ const pool = mysql.createPool({
 
 
 // const test = "SELECT * FROM food f JOIN recipe r ON f.recipe_id = r.id JOIN recipe_ingredient ri ON ri.recipe_id = r.id JOIN ingredient i ON i.id = ri.ingredient_id WHERE f.id = ?" 
+
+// SELECT ri.ingredient_id AS id, i.name, ri.amount FROM recipe_ingredient ri JOIN ingredient i ON ri.ingredient_id = i.id JOIN recipe r ON ri.recipe_id = r.id WHERE ri.recipe_id = 

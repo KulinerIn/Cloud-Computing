09ad968502557c4e660d04387eceec4a0963e938
ClarenceKiva <Clarenceaureliopribadi@gmail.com>
2022-06-08 16:21:36 +0700

    Updated the Query

--- food-api v2/index.js
+++ food-api v2/index.js
@@ -15,8 +15,7 @@ app.get("/", async (req, res) =>{
 });
 
 app.get("/:id", async (req, res) => {
-    const query = "SELECT * FROM food food JOIN recipe r ON food.recipe_id = r.id JOIN recipe_ingredient ri ON ri.recipe_id = r.id JOIN ingredient i ON i.id = ri.ingredient_id WHERE food.id = ?"
-    // const query = "SELECT * FROM food WHERE id = ?";
+    const query = "SELECT * FROM food f JOIN recipe r ON r.id = f.recipe_id WHERE f.id = ?";
     pool.query(query, [ req.params.id ] , (error, results) => {
         // console.log(error);
         // console.log(process.env.DB_NAME);
@@ -27,6 +26,52 @@ app.get("/:id", async (req, res) => {
         }
     });
 });
+
+function success_response(result) {
+    {
+        var data = {
+            id: result.id,
+            name: result.name,
+            alt_name: result.alt_name,
+            image: result.image,
+            description: result.description,
+            origin: result.origin,
+            ingredients: get_recipe_ingredients(result.id)
+        };
+    }
+}
+
+function get_recipe_ingredients(food){
+    food = "SELECT ri.ingredient_id AS id, i.name, ri.amount FROM recipe_ingredient ri JOIN ingredient i USING (id) WHERE ri.recipe_id = r.id"
+}
+
+// const query = await db.query(
+//     sql`
+//       SELECT
+//         f.id,
+//         f.name,
+//         f.alt_name,
+//         f.image,
+//         f.description,
+//         f.origin,
+//         ${nestQuerySingle(
+//           sql`
+//             SELECT r.steps
+//             FROM recipe r WHERE r.id = f.recipe_id
+//           `
+//         )} AS manager,
+//         ${nestQuery(
+//           sql`
+//             SELECT ri.ingredient_id AS id, i.name, ri.amount 
+//             FROM recipe_ingredient ri
+//             JOIN ingredient i USING (id)
+//             WHERE ri.recipe_id = r.id
+//           `
+//         )} AS ingredients
+//       FROM food f
+//       WHERE f.id = ?
+//     `,
+//   );
 
 const pool = mysql.createPool({
     user: process.env.DB_USER,
